0b2017aabad0ccb5a708ab8813d0353d9083b802
Bryan Albuja <balbujal@est.ups.edu.ec>
2022-07-05 10:54:33 -0500

    Avance de barras y cambio de color

--- principal.h
+++ principal.h
@@ -1,6 +1,10 @@
 #ifndef PRINCIPAL_H
 #define PRINCIPAL_H
 
+#include <QPainter>
+#include <QFileDialog>
+#include <QMessageBox>
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -14,15 +18,26 @@ class Principal : public QMainWindow
 public:
     Principal(QWidget *parent = nullptr);
     ~Principal();
+protected:
+    void paintEvent(QPaintEvent *event) override;
 
 private slots:
     void on_actionGuardar_triggered();
 
     void on_pushButton_clicked(bool checked);
 
+    void on_inNota1_valueChanged(int arg1);
+
+    void on_inNota2_valueChanged(int arg1);
+
+    void on_inNota3_valueChanged(int arg1);
+
 private:
     Ui::Principal *ui;
     QPixmap lienzo;
+
+    int getAlto(int valor);
+    int incY(int alto);
 
     void dibujar();
 };
